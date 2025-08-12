8ed1fbdad103b858452f9792e4f10e5c7bee4ddf
Vincenzo Nicassio <tiubarore93@gmail.com>
2025-08-12 18:11:36 +0200

    removed the completedsessions and adjusted the space around

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -230,15 +230,15 @@ const Timer = () => {
       />
 
       <div className="mt-4 p-4 bg-white bg-opacity-80 rounded-xl shadow-inner">
-        <div className="flex justify-between text-sm">
+        <div className="flex justify-around text-sm">
           <div className="text-center">
             <p className="text-gray-500">Today</p>
             <p className="font-medium">{formatTimeForTotals(dailyTotal)}</p>
           </div>
-          <div className="text-center">
+          {/* <div className="text-center">
             <p className="text-gray-500">Sessions</p>
             <p className="font-medium">{sessionsCompleted}</p>
-          </div>
+          </div> */}
           <div className="text-center">
             <p className="text-gray-500">Week</p>
             <p className="font-medium">{formatTimeForTotals(weeklyTotal)}</p>
